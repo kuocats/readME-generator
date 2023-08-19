02f1829de502dacdca75a118554173f2848b4fa5
Vivian Kuo <vivian.kuo4@outlook.com>
2023-08-18 14:56:15 -1000

    edit license selection

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,76 +1,85 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  let licenseType = license.license; // i think this is problem #1
-  let yourLicense = ''
-  if(licenseType === 'MIT') {
-    yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
-  } else if (licenseType === 'GPLv3') {
-    yourLicense = `![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)`
-  } else if (licenseType === 'GPL') {
-    yourLicense = `![GPL license](https://img.shields.io/badge/License-GPL-blue.svg)`
-  } else {
-    license.license = "N/A"
+  switch (license) {
+    case 'MIT License':
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    default:
+      return ``;
   }
-  return yourLicense;
-};
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license, name) {
-  const year = new Date().getFullYear();
-  return(license === "Unlicense") ?
-
-  Anyone is free to copy, modify, publish, use, compile, sell, or
-  distribute this software, either in source code form or as a compiled
-  binary, for any purpose, commercial or non-commercial, and by any
-  means.
-  '
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  return `# ${data.title}
-  
-  ## Description
+// license link for license section, creates link to license type selected by user
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT License':
+      return `[${license}](https://opensource.org/licenses/MIT)`;
+    default:
+      return ``;
+  }
+}
 
+// create license section link in table of contents, only created if license type is provided by user
+function renderLicenseTOC(license) {
+  if (!license) {
+    return '';
+  } else {
+    return `
+ * [License](#license)
+`
+  }
+}
+
+// create license section, only adds license title/section if license type is provided by user
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  } else {
+    return `
+ ## License
+${renderLicenseLink(license)}
+`
+  }
+}
+
+// generate README.md, template literal for markdown file
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  ## Description
   ${data.description}
   
-  ## Table of contents
-  
-  1. [Installation](#installation)
-  2. [Usage](#usage)
-  3. [Tests](#tests)
-  4. [Contribution](#contribution)
-  5. [License](#license)
-  6. [Questions](#questions)
-  
-  # Installation
-  
-  ${data.installation}
-  
-  # Usage
-  
+  ${renderLicenseBadge(data.license)}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+    ${renderLicenseTOC(data.license)}
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  ${data.install}
+
+  ## Usage
   ${data.usage}
   
-  # Tests
-  
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
   ${data.tests}
-  
-  # Contribution
-  
-  ${data.contribution}
 
-  # Questions
+  ## Questions
+  Contact me with any additional questions:
   
-}
+  [https://github.com/${data.username}](https://github.com/${data.username})
 
-// TODO: Create a function to generate markdown for README
-return `# ${data.title}
-
-${renderLicenseBadge(license)} //possible problem #2?
-'
+  ${data.email}
+`;
 }
 
 module.exports = generateMarkdown;
